04629f93dead4d9baabd210aa4f61c6b0d6ab146
Israel-Honorio <israelhcastro11@gmail.com>
2025-04-29 19:56:14 -0300

    Teste 3: implementacao para converter todos os algarismos romanos básicos

--- romanos.cpp
+++ romanos.cpp
@@ -7,6 +7,16 @@ int romanos_para_decimal(char const * num_romano)
     return 1;
   } else if (strcmp(num_romano, "V") == 0) {
     return 5;
+  } else if (strcmp(num_romano, "X") == 0) {
+    return 10;
+  } else if (strcmp(num_romano, "L") == 0) {
+    return 50;
+  } else if (strcmp(num_romano, "C") == 0) {
+    return 100;
+  } else if (strcmp(num_romano, "D") == 0) {
+    return 500;
+  } else if (strcmp(num_romano, "M") == 0) {
+    return 1000;
   }
   return 0; 
 }
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -5,9 +5,12 @@
 
 TEST_CASE( "Numeros romanos - algarismos unicos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("I") == 1 );
-    
     REQUIRE( romanos_para_decimal("V") == 5 );
-
+    REQUIRE( romanos_para_decimal("X") == 10 );
+    REQUIRE( romanos_para_decimal("L") == 50 );
+    REQUIRE( romanos_para_decimal("C") == 100 );
+    REQUIRE( romanos_para_decimal("D") == 500 );
+    REQUIRE( romanos_para_decimal("M") == 1000 );
     
 }
 
